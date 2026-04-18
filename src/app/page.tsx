4254bffa66349736ef6f4d6432b1aaf7a00590c4
Ghansham Deepak Gavande <ghansham.deepak.gavande@ibm.com>
2026-04-18 21:09:40 +0530

    chore: remove unwanted CreativePhilosophy section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import CinematicHero from "@/components/CinematicHero";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
-import Skills3D from "@/components/Skills3D";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="bg-black selection:bg-white/30 overflow-x-hidden">
+    <main className="bg-black selection:bg-white/30">
       {/* 1. Fully pinned canvas Image sequence playing on scroll */}
       <CinematicHero />
 
@@ -16,10 +15,7 @@ export default function Home() {
       {/* 3. Follows identically right after the hero sequence releases */}
       <Projects />
       
-      {/* 4. Massive Flowing 3D CSS Spheres containing Skills mapped purely to GSAP */}
-      <Skills3D />
-
-      {/* 5. The grand finale contact/social component seamlessly appended */}
+      {/* 4. The grand finale contact/social component seamlessly appended */}
       <Footer />
     </main>
   );
